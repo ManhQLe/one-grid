5d62fb8957b599904fd348c33d65f215f6fe3e05
Manh Le <quang.manh@gmail.com>
2019-08-18 16:20:15 -0500

    First blood

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import GridAgr from './GridAgr'
+
+
+
+class OneGrid {        
+    constructor(agrs){        
+        this.agrs = new GridAgr(agrs)
+        
+    }
+
+    render(){
+    }
+}
+
+
+export default OnGrid
